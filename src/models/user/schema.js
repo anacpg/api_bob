00ccc1cdca6269efe8cb993d87b9ce6167081083
anacpg <anacris.pg91@gmail.com>
2018-12-08 10:22:45 +0100

    [improvement] Add the user model

---
+++ src/models/user/schema.js
@@ -0,0 +1,15 @@
+import { Schema } from 'mongoose';
+import { C, REGEXPS } from '../../common';
+import { RandomProvider } from '../../providers';
+import { STRING, NUMBER } from '../common/types';
+
+module.exports = new Schema({
+  id: STRING({ default: RandomProvider.token, unique: true }),
+  name: STRING(),
+  surname: STRING(),
+  bags: NUMBER(),
+  state: STRING({ default: C.USER_STATES.ACTIVE, enum: Object.keys(C.USER_STATES), null: true }),
+  username: STRING({ unique: true }),
+  email: STRING({ unique: true, match: REGEXPS.EMAIL }),
+  password: STRING(),
+});
