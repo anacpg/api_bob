8d813c9b446eebc82e580c0ee23d7f4701bb21e2
anacpg <anacris.pg91@gmail.com>
2018-12-08 09:59:21 +0100

    [improvement] Add server

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { LogProvider } from './providers';
+// import routes from './routes';
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const APP_PORT = 4000;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bob', { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.end();
+  }
+  next();
+  
+});
+
+app.listen(APP_PORT, () => {
+  LogProvider.info(`App listening on port ${APP_PORT}`); // eslint-disable-line no-console
+});
+
+// app.use('/', routes);
